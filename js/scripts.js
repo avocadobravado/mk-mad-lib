9ba1386d0117ff68994543fb13fbb9715dad875d
Daniel Lopez and Michael Koceja <michael@>
2017-04-26 09:24:17 -0700

    add travel, animal, and mythic inputs. extend #story

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,17 @@
 $(document).ready(function() {
   $("#form1").submit(function(event) {
     var person1Input = $("input#person1").val();
+    var person2Input = $("input#person2").val();
+    var travelInput = $("input#travel").val();
+    var animalInput = $("input#animal").val();
+    var mythicalInput = $("input#mythical").val();
+
 
     $(".person1").text(person1Input);
+    $(".person2").text(person2Input);
+    $(".travel").text(travelInput);
+    $(".animal").text(animalInput);
+    $(".mythical").text(mythicalInput);
 
     $("#story").show();
 
